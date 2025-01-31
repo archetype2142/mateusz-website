607a4f249a37d5eea1c4b33b9c473a7e262049d6
Ritwick Malhotra <malhotraritwick2011@gmail.com>
2025-01-31 14:34:52 +0100

    fixed rubocop linted

--- config/initializers/spina.rb
+++ config/initializers/spina.rb
@@ -3,7 +3,7 @@ Spina.configure do |config|
   # ===============
   # All locales your content should be available in.
   # Defaults to I18n.default_locale
-  config.locales = [:en, :pl]
+  config.locales = [ :en, :pl ]
 
   # Backend title
   # ===============
